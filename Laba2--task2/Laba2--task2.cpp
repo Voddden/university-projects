5089e2716c233b612c538fd64ce7b01fcc054f70
Danik Vodnev <voddden@gmail.com>
2021-09-21 00:04:17 +0300

    Сделал 2-ое задание

--- Laba2--task2/Laba2--task2.cpp
+++ Laba2--task2/Laba2--task2.cpp
@@ -1,20 +1,49 @@
-﻿// Laba2--task2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿//Лабораторная 2, задача 2. Выполнена: Воднев Д. И.
+
+/*
+Меньшее из двух значений переменных целого типа Х и Y заменить
+нулём, а в случае их равенства - заменить нулями оба; наибольшее из трёх
+различных значений переменных вещественного типа А, В и С уменьшите на K.
+K вводится с клавиатуры.
+*/
 
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int X, Y;
+    cout << "Введите 2 целых числа\n";
+    cin >> X >> Y;
+    if (X > Y) {
+        Y == 0;
+        cout << "Y меньше чем X. Теперь Y = 0\n\n";
+    }
+    else if (Y > X) {
+        X == 0;
+        cout << "X меньше чем Y. Теперь X = 0\n\n";
+    }
+    else if (X == Y) {
+        X == 0;
+        Y == 0;
+        cout << "X = Y. Теперь X и Y равны нулю\n\n";
+    }
+
+    double A, B, C, K;
+    cout << "Введите 3 вещественных числа A B C и число K\n";
+    cin >> A >> B >> C >> K;
+    if (A > B && A > C) {
+        A = A - K;
+        cout << "A - наибольшее число. Теперь А = " << A << endl;
+    }
+    else if (B > A && B > C) {
+        B = B - K;
+        cout << "B - наибольшее число. Теперь B = " << B << endl;
+    }
+    else if (C > A && C > B) {
+        C = C - K;
+        cout << "C - наибольшее число. Теперь C = " << C << endl;
+    }
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
