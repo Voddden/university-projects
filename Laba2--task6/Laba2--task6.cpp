f7ac8b667324afd312b755810358df3ed324bc45
Danik Vodnev <voddden@gmail.com>
2021-09-22 21:30:56 +0300

    Сделал 6-ое задание (осталось проетстировать)

---
+++ Laba2--task6/Laba2--task6.cpp
@@ -0,0 +1,32 @@
+﻿//Лабораторная 2, задача 6. Выполнена: Воднев Д. И.
+
+/*
+    Составить программу используя переключатель switch:
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    double a, b, c, Y;
+    char N;
+    a = -13.8;
+    b = 8.9;
+    c = 25;
+    cout << "Введите переменную N\n";
+    cin >> N;
+    switch (N) {
+        case '2' :
+            cout << "Y равно " << b * c - a * a << endl;
+        case '56' : 
+            cout << "Y равно " << b * c << endl;
+        case '7' :
+            cout << "Y равно " << a * a + c << endl;
+        case '3' :
+            cout << "Y равно " << a - b * c << endl;
+        default :
+            cout << "Y равно " << pow(a + b, 3) << endl;
+    }
+    return 0;
+}
