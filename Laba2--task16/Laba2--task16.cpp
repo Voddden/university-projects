b2f4226e271e3d142feb126bc2f6cdf2c01a29d3
Danik Vodnev <voddden@gmail.com>
2021-09-28 17:31:40 +0300

    Правки в 16 задание

---
+++ Laba2--task16/Laba2--task16.cpp
@@ -0,0 +1,36 @@
+﻿//Лабораторная 2, задача 16. Выполнена: Воднев Д. И.
+
+/*
+    Введены два числа. Выведите их НОД и НОК.
+*/
+
+/*
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    return 0;
+}
+*/
+
+
+
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {
+    return (a == 0) ? b : gcd(b % a, a);
+}
+
+int main() {
+    int n, m = 0;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int a;
+        cin >> a;
+        m = gcd(m, a);
+    }
+    cout << m;
+    return 0;
+}
