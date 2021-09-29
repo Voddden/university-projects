312b4c576933e386fc2592450fc448ac4c86f0ab
Danik Vodnev <voddden@gmail.com>
2021-09-29 21:31:44 +0300

    Разбил задание 7 на файлы 7.1, 7.2, 7.3

---
+++ Laba2--task7.1/Laba2--task7.1.cpp
@@ -0,0 +1,44 @@
+﻿//Лабораторная 2, задача 7.1 Выполнена: Воднев Д. И.
+
+/*
+    Составить программу для решения уравнения:
+    7.1 ax^4 + bx^2 + c = 0;
+*/
+
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    cout << "7.1 ax^4 + bx^2 + c = 0\n\n";
+    double a, b, c;
+    cout << "Введите переменные a, b, c\n";
+    cin >> a >> b >> c;
+    double D; // дискриминант
+    D = b * b - 4 * a * c;
+    if (a == 0) {
+        cout << "Ошибка! a не может быть равен нулю\n";
+    }
+    else if (D > 0) {
+        double z1, z2;
+        z1 = (-b + sqrt(D)) / (2 * a);
+        z2 = (-b - sqrt(D)) / (2 * a);
+        double x1, x2;
+        x1 = sqrt(z1);
+        x2 = sqrt(z2);
+        cout << "Дискриминант больше нуля. Корни уравнения: x1 = " << x1 << ", x2 = " << x2 << endl;
+    }
+    else if (D == 0) {
+        double z1 = (-b) / (2 * a);
+        double x1 = sqrt(z1);
+        cout << "Дискриминант равен нулю. Корень уравнения: x1 = " << x1 << endl;
+    }
+    else if (D < 0) {
+        cout << "Уравнение не имеет корней\n";
+    }
+    else {
+        cout << "Возникла ошибка\n";
+    }
+    return 0;
+}
---
+++ Laba2--task7.3/Laba2--task7.3.cpp
@@ -0,0 +1,15 @@
+﻿//Лабораторная 2, задача 7.3 Выполнена: Воднев Д. И.
+
+/*
+    Составить программу для решения уравнения:
+    7.3 x^3 + px + q = 0
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+
+    return 0;
+}
