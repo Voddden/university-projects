f0a585eb70881f29dff9478c0cc2e7816949ee01
Danik Vodnev <voddden@gmail.com>
2021-09-30 13:43:25 +0300

    Начал делать 8 задание

--- Laba2--task8/Laba2--task8.cpp
+++ Laba2--task8/Laba2--task8.cpp
@@ -12,9 +12,39 @@
 
 #include <iostream>
 using namespace std;
+#include <cmath>
 
 int main() {
     setlocale(LC_ALL, "Rus");
+    int n;
+    cout << "Введите 1, 2 или 3 чтобы выбрать функцию\n 1 --- 2x \n 2 --- x^3 \n 3 --- x/3\n";
+    cin >> n;
 
+    //const double exp = 2.718281828459;
+    switch (n) { 
+        case 1: 
+            double a, b, z, x, y;
+            cout << "Введите переменные a, b, z в этом же порядке\n";
+            cin >> a >> b >> z;
+            if (z < -1) {
+                x = (-z) / 3;
+
+                y = a * 2 * x - log(x + 2.5) + b * (exp(x) - exp(-x));
+                cout << "Значение выражения a * 2x - ln(x + 2.5) + b(e^x - e^(-x)) равно " << y << endl;
+            }
+            if (z >= -1) {
+                x = fabs(z);
+
+                y = a * 2 * x - log(x + 2.5) + b * (exp(x) - exp(-x));
+                cout << "Значение выражения a * 2x - ln(x + 2.5) + b(e^x - e^(-x)) равно " << y << endl;
+            }
+            break;
+        case 2:
+            cout << "Вы выбрали 2\n";
+            break;
+        case 3:
+            cout << "Вы выбрали 3\n";
+            break;
+    }
     return 0;
 }
