4c1d9dfc7dff952c9130c96e0743758ca040debf
Danik Vodnev <voddden@gmail.com>
2021-10-09 13:04:18 +0300

    Закончил 16 задание

--- Laba2--task16/Laba2--task16.cpp
+++ Laba2--task16/Laba2--task16.cpp
@@ -1,22 +1,30 @@
 ﻿//Лабораторная 2, задача 16. Выполнена: Воднев Д. И.
 
 /*
-    Введены два числа. Выведите их НОД и НОК.
+	Введены два числа. Выведите их НОД и НОК.
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Rus");
-    int a, b;
-    while (1) {
-        cout << "Введите a\n";
-        cin >> a;
-        cout << "Введите b\n";
-        cin >> b;
-        
-    }
-
-    return 0;
+	setlocale(LC_ALL, "Rus");
+	int a, b, a1, b1, NOD, NOK;
+	cout << "Введите переменые a, b\n";
+	cin >> a1 >> b1;
+	a = a1;
+	b = b1;
+	while (a != 0 && b != 0) {
+		if (a > b) {
+			a = a % b;
+		}
+		else {
+			b = b % a;
+		}
+	}
+	NOD = a + b;
+	NOK = (a1 * b1) / NOD;
+	cout << endl << "НОД этих чисел равен " << NOD << endl;
+	cout << "НОК этих чисел равен " << NOK << endl;
+	return 0;
 }
