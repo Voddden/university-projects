e1fddc66231f0081834c4b9ae749e5b9c68945b0
Даник <voddden@gmail.com>
2021-10-10 16:58:31 +0300

    Попытка фикса 7.2

--- Laba2--task7.2/Laba2--task7.2.cpp
+++ Laba2--task7.2/Laba2--task7.2.cpp
@@ -27,12 +27,12 @@ int main() {
         c1 = c - 2 * a;
         D1 = b1 * b1 - 4 * a1 * c1;
         if (D1 < 0) {
-            //cout << "Решений нет\n";
+            cout << "Решений нет\n";
         }
-        else if (D1 == 0) {
-            cout << "error";
-        }
-        else {
+        //else if (D1 == 0) {
+        //    cout << "error";
+        //}
+        else if (D1 >= 0) {
             x1_1 = (-b1 + sqrt(D1)) / (2 * a1);
             x1_2 = (-b1 - sqrt(D1)) / (2 * a1);
 
@@ -45,32 +45,31 @@ int main() {
             if (D2 < 0) {
                 cout << "Решений нет\n";
             }
-            else if (D2 == 0) {
-                cout << "error1\n";
-            }
-            else {
+            //else if (D2 == 0) {
+            //    cout << "error1\n";
+            //}
+            else if (D2 >= 0) {
                 x2_1 = (-b2 + sqrt(D2)) / (2 * a2);
                 x2_2 = (-b2 - sqrt(D2)) / (2 * a2);
                 cout << "Корни уравнения:\n" << x2_1 << endl << x2_2 << endl;
+            }
 
-                // 3-ее уравнение
-
-                double a3, b3, c3, D3, x3_1, x3_2;
-                a3 = 1;
-                b3 = -x1_2;
-                c3 = 1;
-                D3 = b3 * b3 - 4 * a3 * c3;
-                if (D3 < 0) {
-                    cout << "Решений нет\n";
-                }
-                else if (D3 == 0) {
-                    cout << "error3\n";
-                }
-                else {
-                    x3_1 = (-b3 + sqrt(D3)) / (2 * a3);
-                    x3_2 = (-b3 - sqrt(D3)) / (2 * a3);
-                    cout << x3_1 << endl << x3_2 << endl;
-                }
+            // 3-ее уравнение
+            double a3, b3, c3, D3, x3_1, x3_2;
+            a3 = 1;
+            b3 = -x1_2;
+            c3 = 1;
+            D3 = b3 * b3 - 4 * a3 * c3;
+            if (D3 < 0) {
+                cout << "Решений нет\n";
+            }
+            //else if (D3 == 0) {
+            //    cout << "error3\n";
+            //}
+            else if (D3 >= 0) {
+                x3_1 = (-b3 + sqrt(D3)) / (2 * a3);
+                x3_2 = (-b3 - sqrt(D3)) / (2 * a3);
+                cout << x3_1 << endl << x3_2 << endl;
             }
         }
     }
