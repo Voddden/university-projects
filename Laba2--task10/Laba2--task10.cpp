9c46c596f08e9c2cc04cae749b9b076699a55daf
Даник <voddden@gmail.com>
2021-10-11 13:59:00 +0300

    начал делать 10 задание

--- Laba2--task10/Laba2--task10.cpp
+++ Laba2--task10/Laba2--task10.cpp
@@ -15,22 +15,24 @@ int main() {
     const double pi = 3.1415926535;
     cout << "Введите значение x\n";
     cin >> x;
-    if (x > 0 && x < (5 * pi) / 12) {
-        cout << "Функция lnx с этим значением x является наименьшей\n";
-    }
-    else {
-        //x = x / 180;
-        cosx = (pi * pi - 4 * x * x) / (pi * pi + x * x);
-        sinx = (16 * x * (pi - x)) / (5 * pi * pi - 4 * x * (pi - x));
-        if (sinx < cosx) {
-            cout << "Функция sinx с этим значением x является наименьшей\n";
-        }
-        if (sinx > cosx) {
-            cout << "Функция cosx с этим значением x является наименьшей\n";
-        }
-    }
 
+
+
+
+
+    cosx = (pi * pi - 4 * x * x) / (pi * pi + x * x);
+    sinx = (16 * x * (pi - x)) / (5 * pi * pi - 4 * x * (pi - x));
+    if (sinx < cosx < lnx) {
+        cout << "Функция sinx с этим значением x является наименьшей\n";
+    }
+    else if (cosx > sinx > lnx) {
+        cout << "Функция cosx с этим значением x является наименьшей\n";
+    }
+    else if (lnx > sinx > cosx) {
+
+    }
     //cout << (sinx * sinx + cosx * cosx) << endl;
+
 
 
     return 0;
