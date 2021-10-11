840e87a11e5e2279529d6bf2bdd77a5524952590
Даник <voddden@gmail.com>
2021-10-11 13:49:27 +0300

    Закончил 7.3

--- Laba2--task7.3/Laba2--task7.3.cpp
+++ Laba2--task7.3/Laba2--task7.3.cpp
@@ -15,7 +15,7 @@ int main() {
     const double pi = 3.1415926535;
     setlocale(LC_ALL, "Rus");
     cout << "Введите p и q\n";
-    double a = 0, b, c;
+    double a = 1, b, c;
     cin >> b >> c;
     double Q, R, S;
     Q = (a * a - 3 * b) / 9;
@@ -28,23 +28,19 @@ int main() {
         x1 = -2 * sqrt(Q) * cos(o) - a / 3;
         x2 = -2 * sqrt(Q) * cos(o + 2 * pi / 3) - a / 3;
         x3 = -2 * sqrt(Q) * cos(o - 2 * pi / 3) - a / 3;
-        cout << "Решения уравнения: " << x1 << " " << x2 << " " << x3 << endl;
+        cout << "1Корни уравнения:\n" << x1 << endl << x2 << endl << x3 << endl;
     }
     if (S < 0) {
-        if (Q > 0) {
-            double o, x1;
-            //o = 1 / 3 * ach(fabs(R) / sqrt(Q * Q * Q));
-        }
-        if (Q < 0) {
-
-        }
-        if (Q == 0) {
-
-        }
+        double p = b, q = c;
+        double x1;
+        x1 = cbrt((-q / 2) + sqrt(q * q / 4 + p * p * p / 27)) + cbrt((-q / 2) - sqrt((q * q / 4) + p * p * p / 27));
+        cout << "Корень уравнения:\n" << x1 << endl;
     }
     if (S == 0) {
-
+        double x1, x2;
+        x1 = -2 * pow(R, 1 / 3) - a / 3;
+        x2 = pow(R, 1 / 3) - a / 3;
+        cout << "Корни уравнения:\n" << x1 << endl << x2 << endl;
     }
-
     return 0;
 }
