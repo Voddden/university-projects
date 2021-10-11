5fe467927b05a25c7d9ffa426104f5df12221299
Даник <voddden@gmail.com>
2021-10-12 01:07:34 +0300

    100% рабочий билд 10-го задания

--- Laba2--task10/Laba2--task10.cpp
+++ Laba2--task10/Laba2--task10.cpp
@@ -1,261 +1,469 @@
 ﻿//Лабораторная 2, задача 10. Выполнена: Воднев Д. И.
 
 /*
-    Из величин, определяемых выражениями a = sinx, b = cosx, c = ln|x|
-    при заданном х, определить и вывести на экран дисплея минимальное
-    значение. Библиотеку <cmath> использовать запрещено.
+	Из величин, определяемых выражениями a = sinx, b = cosx, c = ln|x|
+	при заданном х, определить и вывести на экран дисплея минимальное
+	значение. Библиотеку <cmath> использовать запрещено.
 */
 
 #include <iostream>
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "Rus");
-    const double pi = 3.1415926535;
+	setlocale(LC_ALL, "Rus");
+	const double pi = 3.1415926535;
 
-    cout << "Введите x\n";
-    double x;
-    cin >> x;
+	cout << "Введите x\n";
+	double x;
+	cin >> x;
 
-    double LNX, SINX, COSX;
-    
-    if (x == 0) {
-        cout << "Ошибка, x не может быть равен нулю\n";
-    }
-    else if (x > 0) {
-        
-        //// ln - 1
-        double count = 1, totalValue = 0, it = 1000, xLN = x, z = 1, powe = 1, y;
-        if (xLN < 0) {
-            xLN = -xLN;
-            while (count <= it)
-            {
-                for (int i = 0; i < powe; i++) {
-                    z *= (x - 1) / (x + 1);
-                }
-                y = (1 / powe) * z;
+	double LNX, SINX, COSX;
 
-                totalValue = totalValue + y;
-                powe = powe + 2;
-                count++;
-                z = 1;
-               
-            }
-            double lnx;
-            lnx = 2 * totalValue;
-            cout << "lnx: " << lnx << endl;
-            
-        }
-        else {
-            while (count <= it)
-            {
-                for (int i = 0; i < powe; i++) {
-                    z *= (x - 1) / (x + 1);
-                }
-                y = (1 / powe) * z;
+	if (x == 0) {
+		cout << "Ошибка, x не может быть равен нулю\n";
+	}
+	else if (x > 0) {
 
-                totalValue = totalValue + y;
-                powe = powe + 2;
-                count++;
-                z = 1;
-            }
-            double lnx;
-            lnx = 2 * totalValue;
-            cout << "lnx: " << lnx << endl;
-            
-        }
-        //// ln - 0
+		//// ln - 1
+		double count = 1, totalValue = 0, it = 1000, xLN = x, z = 1, powe = 1, y;
+		if (xLN < 0) {
+			xLN = -xLN;
+			while (count <= it)
+			{
+				for (int i = 0; i < powe; i++) {
+					z *= (x - 1) / (x + 1);
+				}
+				y = (1 / powe) * z;
 
-        //// sinx - 1
+				totalValue = totalValue + y;
+				powe = powe + 2;
+				count++;
+				z = 1;
 
+			}
+			double lnx;
+			lnx = 2 * totalValue;
+			//cout << "lnx: " << lnx << endl;
+			LNX = lnx;
 
-        if (x > 0 && x < pi / 2) { // 1 четверть +
-            double xSIN = x;
-            if (xSIN > pi) {
-                //xSIN = xSIN - pi;
-                while (xSIN > pi) {
-                    xSIN = xSIN - pi;
-                }
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << sinx << endl;
-            }
-            else {
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << sinx << endl;
-            }
-        }
-        else if (x >= pi / 2 && x < pi) { // 2 четверть +
-            double xSIN = x;
-            if (xSIN > pi) {
-                //xSIN = xSIN - pi;
-                while (xSIN > pi) {
-                    xSIN = xSIN - pi;
-                }
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << sinx << endl;
-            }
-            else {
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << sinx << endl;
-            }
-        }
-        else if (x >= pi && x < 1.5 * pi) { // 3 четверть -
-            double xSIN = x;
-            if (xSIN > pi) {
-                //xSIN = xSIN - pi;
-                while (xSIN > pi) {
-                    xSIN = xSIN - pi;
-                }
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << -sinx << endl;
-            }
-            else {
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << -sinx << endl;
-            }
-        }
-        else if (x >= 1.5 * pi && x < 2 * pi) { // 4 четверть -
-            double xSIN = x;
-            if (xSIN > pi) {
-                //xSIN = xSIN - pi;
-                while (xSIN > pi) {
-                    xSIN = xSIN - pi;
-                }
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << -sinx << endl;
-            }
-            else {
-                double sinx;
-                sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
-                cout << "sinx: " << -sinx << endl;
-            }
-        }
+		}
+		else {
+			while (count <= it)
+			{
+				for (int i = 0; i < powe; i++) {
+					z *= (x - 1) / (x + 1);
+				}
+				y = (1 / powe) * z;
 
+				totalValue = totalValue + y;
+				powe = powe + 2;
+				count++;
+				z = 1;
+			}
+			double lnx;
+			lnx = 2 * totalValue;
+			//cout << "lnx: " << lnx << endl;
+			LNX = lnx;
+		}
+		//// ln - 0
 
+		//// sinx - 1
+		double xSIN1 = x;
+		while (xSIN1 > 2 * pi) {
+			xSIN1 = xSIN1 - 2 * pi;
+		}
 
-
-        //// sinx - 0
-        
-        //// cosx - 1
-
-
-        if (x > 0 && x < pi / 2) { // 1 четверть +
-            double xCOS = x;
-            if (xCOS > pi) {
-                //xCOS = xCOS - pi;
-                while (xCOS > pi) {
-                    xCOS = xCOS - pi;
-                }
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << cosx << endl;
-            }
-            else {
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << cosx << endl;
-            }
-        }
-        else if (x >= pi / 2 && x < pi) { // 2 четверть -
-            double xCOS = x;
-            if (xCOS > pi) {
-                //xCOS = xCOS - pi;
-                while (xCOS > pi) {
-                    xCOS = xCOS - pi;
-                }
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << -cosx << endl;
-            }
-            else {
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << -cosx << endl;
-            }
-        }
-        else if (x >= pi && x < 1.5 * pi) { // 3 четверть -
-            double xCOS = x;
-            if (xCOS > pi) {
-                //xCOS = xCOS - pi;
-                while (xCOS > pi) {
-                    xCOS = xCOS - pi;
-                }
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << -cosx << endl;
-            }
-            else {
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << -cosx << endl;
-            }
-        }
-        else if (x >= 1.5 * pi && x < 2 * pi) { // 4 четверть +
-            double xCOS = x;
-            if (xCOS > pi) {
-                //xCOS = xCOS - pi;
-                while (xCOS > pi) {
-                    xCOS = xCOS - 2*pi;
-                }
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << cosx << endl;
-            }
-            else {
-                double cosx;
-                cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
-                cout << "cosx: " << cosx << endl;
-            }
-        }
-
-        
-
-        //// cosx - 0
-
-        /*
-        double angX = 180 / pi * x;
-        if (angX > 180) {
-            angX = angX - 180;
-        }
-        sinx = (4 * angX * (180 - angX )) / (40500 - angX * (180 - angX));
-        */
-        //x > pi ? x = x - pi : cout << 101;
-
-        
-        
-        //cosx = 0;
-        //cout << "cosx: " << cosx << endl << "sinx: " << sinx / 2 << endl << "lnx: " << lnx << endl;
-
-        /*
-        /////////////////////////////////////////// сравнение значений
-        if (sinx < cosx < lnx) {
-            cout << "Функция sinx является наименьшей, и равной " << sinx << endl;
-        }
-        else if (cosx > sinx > lnx) {
-            cout << "Функция cosx является наименьшей, и равной " << cosx << endl;
-        }
-        else if (lnx > sinx > cosx) {
-            cout << "Функция lnx является наименьшей, и равной " << lnx << endl;
-        }
-        else {
-            cout << "Ошибка\n";
-        }
-        */
-    }
-    else if (x < 0) {
-        
-    }
+		if (xSIN1 > 0 && xSIN1 < pi / 2) { // 1 четверть +
+			double xSIN = x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = sinx;
+			}
+		}
+		else if (xSIN1 >= pi / 2 && xSIN1 < pi) { // 2 четверть +
+			double xSIN = x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = sinx;
+			}
+		}
+		else if (xSIN1 >= pi && xSIN1 < 1.5 * pi) { // 3 четверть -
+			double xSIN = x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = -sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = -sinx;
+			}
+		}
+		else if (xSIN1 >= 1.5 * pi && xSIN1 < 2 * pi) { // 4 четверть -
+			double xSIN = x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = -sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = -sinx;
+			}
+		}
+		//// sinx - 0
 
 
-    
+		//// cosx - 1
+		double xCOS1 = x;
+		while (xCOS1 > 2 * pi) {
+			xCOS1 = xCOS1 - 2 * pi;
+		}
+
+		if (xCOS1 > 0 && xCOS1 < pi / 2) { // 1 четверть +
+			double xCOS = x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+		}
+		else if (xCOS1 >= pi / 2 && xCOS1 < pi) { // 2 четверть -
+			double xCOS = x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+			else {
+				double cosx;
+				xCOS = xCOS - pi;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+		}
+		else if (xCOS1 >= pi && xCOS1 < 1.5 * pi) { // 3 четверть -
+			double xCOS = x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+		}
+		else if (xCOS1 >= 1.5 * pi && xCOS1 < 2 * pi) { // 4 четверть +
+			double xCOS = x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - 2 * pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+		}
+		//// cosx - 0
+
+		
+		//cout << "lnx: " << LNX << endl << "sinx: " << SINX << endl << "cosx: " << COSX << endl << endl;
+		
+
+		if (LNX < SINX && LNX < COSX) {
+			cout << "Функция lnx является наименьшей, и равной " << LNX << endl;
+		}
+		else if (SINX < LNX && SINX < COSX) {
+			cout << "Функция sinx является наименьшей, и равной " << SINX << endl;
+		}
+		else if (COSX < LNX && COSX < SINX) {
+			cout << "Функция cosx является наименьшей, и равной " << COSX << endl;
+		}
+	}
+	else if (x < 0) {
+
+		//// ln - 1
+		double count = 1, totalValue = 0, it = 1000, xLN = -x, z = 1, powe = 1, y;
+		if (xLN < 0) {
+			xLN = -xLN;
+			while (count <= it)
+			{
+				for (int i = 0; i < powe; i++) {
+					z *= (-x - 1) / (-x + 1);
+				}
+				y = (1 / powe) * z;
+
+				totalValue = totalValue + y;
+				powe = powe + 2;
+				count++;
+				z = 1;
+
+			}
+			double lnx;
+			lnx = 2 * totalValue;
+			//cout << "lnx: " << lnx << endl;
+			LNX = lnx;
+
+		}
+		else {
+			while (count <= it)
+			{
+				for (int i = 0; i < powe; i++) {
+					z *= (-x - 1) / (-x + 1);
+				}
+				y = (1 / powe) * z;
+
+				totalValue = totalValue + y;
+				powe = powe + 2;
+				count++;
+				z = 1;
+			}
+			double lnx;
+			lnx = 2 * totalValue;
+			//cout << "lnx: " << lnx << endl;
+			LNX = lnx;
+		}
+		//// ln - 0
 
 
-    return 0;
+		//// sinx - 1
+		double xSIN1 = -x;
+		while (xSIN1 > 2 * pi) {
+			xSIN1 = xSIN1 - 2 * pi;
+		}
+
+		if (xSIN1 > 0 && xSIN1 < pi / 2) { // 1 четверть +
+			double xSIN = -x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = -sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = -sinx;
+			}
+		}
+		else if (xSIN1 >= pi / 2 && xSIN1 < pi) { // 2 четверть +
+			double xSIN = -x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = -sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << sinx << endl;
+				SINX = -sinx;
+			}
+		}
+		else if (xSIN1 >= pi && xSIN1 < 1.5 * pi) { // 3 четверть -
+			double xSIN = -x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = sinx;
+			}
+		}
+		else if (xSIN1 >= 1.5 * pi && xSIN1 < 2 * pi) { // 4 четверть -
+			double xSIN = -x;
+			if (xSIN > pi) {
+				while (xSIN > pi) {
+					xSIN = xSIN - pi;
+				}
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = sinx;
+			}
+			else {
+				double sinx;
+				sinx = (16 * xSIN * (pi - xSIN)) / (5 * pi * pi - 4 * xSIN * (pi - xSIN));
+				//cout << "sinx: " << -sinx << endl;
+				SINX = sinx;
+			}
+		}
+		//// sinx - 0
+
+
+		//// cosx - 1
+		double xCOS1 = -x;
+		while (xCOS1 > 2 * pi) {
+			xCOS1 = xCOS1 - 2 * pi;
+		}
+
+		if (xCOS1 > 0 && xCOS1 < pi / 2) { // 1 четверть +
+			double xCOS = -x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+		}
+		else if (xCOS1 >= pi / 2 && xCOS1 < pi) { // 2 четверть -
+			double xCOS = -x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+			else {
+				double cosx;
+				xCOS = xCOS - pi;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+		}
+		else if (xCOS1 >= pi && xCOS1 < 1.5 * pi) { // 3 четверть -
+			double xCOS = -x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << -cosx << endl;
+				COSX = -cosx;
+			}
+		}
+		else if (xCOS1 >= 1.5 * pi && xCOS1 < 2 * pi) { // 4 четверть +
+			double xCOS = -x;
+			if (xCOS > pi) {
+				while (xCOS > pi) {
+					xCOS = xCOS - 2 * pi;
+				}
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+			else {
+				double cosx;
+				cosx = (pi * pi - 4 * xCOS * xCOS) / (pi * pi + xCOS * xCOS);
+				//cout << "cosx: " << cosx << endl;
+				COSX = cosx;
+			}
+		}
+		//// cosx - 0
+
+		
+		//cout << "lnx: " << LNX << endl << "sinx: " << SINX << endl << "cosx: " << COSX << endl << endl;
+		
+
+		if (LNX < SINX && LNX < COSX) {
+			cout << "Функция lnx является наименьшей, и равной " << LNX << endl;
+		}
+		else if (SINX < LNX && SINX < COSX) {
+			cout << "Функция sinx является наименьшей, и равной " << SINX << endl;
+		}
+		else if (COSX < LNX && COSX < SINX) {
+			cout << "Функция cosx является наименьшей, и равной " << COSX << endl;
+		}
+	}
+	return 0;
 }
