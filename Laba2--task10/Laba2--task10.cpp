783954c177568a3f83936d0d70b36cfdefa062e6
Даник <voddden@gmail.com>
2021-10-11 18:08:08 +0300

    Доделал логарифм. Осталось пофиксить синус и косинус

--- Laba2--task10/Laba2--task10.cpp
+++ Laba2--task10/Laba2--task10.cpp
@@ -11,53 +11,77 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    // ln
     double count = 1, totalValue = 0, it, x, z = 1, powe = 1, y;
     it = 1000;
     cout << "Введите x\n";
     cin >> x;
-    while (count <= it)
-    {
-        for (int i = 0; i < powe; i++) {
-            z *= (x - 1) / (x + 1);
-        }
-        y = (1 / powe) * z;
-
-        totalValue = totalValue + y;
-        powe = powe + 2;
-        count++;
-        z = 1;
-    }
-
-    //cout << "Значение логарифма:\n" << 2 * totalValue << endl;
-    // ln
-
-    
-    double sinx, cosx, lnx;
-
-    lnx = totalValue;
-    const double pi = 3.1415926535;
-    //cout << "Введите значение x\n";
-    //cin >> x;
-    cosx = (pi * pi - 4 * x * x) / (pi * pi + x * x);
-    sinx = (16 * x * (pi - x)) / (5 * pi * pi - 4 * x * (pi - x));
-
-    // сравнение значений
-    if (sinx < cosx < lnx) {
-        cout << "Функция sinx с этим значением x является наименьшей, и равной " << sinx << endl;
-    }
-    else if (cosx > sinx > lnx) {
-        cout << "Функция cosx с этим значением x является наименьшей\n";
-    }
-    else if (lnx > sinx > cosx) {
-
+    if (x == 0) {
+        cout << "Ошибка, x не может быть равен нулю\n";
     }
     else {
-        cout << "Ошибка\n";
-    }
-    
 
-    //cout << (sinx * sinx + cosx * cosx) << endl;
+        //////////// ln - начало
+
+        
+        //double x1; // x для работы на вычисление логарифма
+        if (x < 0) {
+            x = -x;
+        }
+        
+        while (count <= it)
+        {
+            for (int i = 0; i < powe; i++) {
+                z *= (x - 1) / (x + 1);
+            }
+            y = (1 / powe) * z;
+
+            totalValue = totalValue + y;
+            powe = powe + 2;
+            count++;
+            z = 1;
+        }
+        /////////// ln - конец
+        double sinx, cosx, lnx;
+        lnx = 2 * totalValue;
+        x = -x;
+        //////////  sin cos start
+
+        const double pi = 3.1415926535;
+        double angX = 180 / pi * x;
+        if (angX > 180) {
+            angX = angX - 180;
+        }
+
+        
+
+        sinx = (4 * angX * (180 - angX )) / (40500 - angX * (180 - angX));
+        
+
+        //cosx = (pi * pi - 4 * x * x) / (pi * pi + x * x);
+        //sinx = (16 * x * (pi - x)) / (5 * pi * pi - 4 * x * (pi - x));
+
+        cosx = 0;
+        cout << "cosx: " << cosx << endl << "sinx: " << sinx << endl << "lnx: " << lnx << endl;
+
+        /*
+        /////////////////////////////////////////// сравнение значений
+        if (sinx < cosx < lnx) {
+            cout << "Функция sinx является наименьшей, и равной " << sinx << endl;
+        }
+        else if (cosx > sinx > lnx) {
+            cout << "Функция cosx является наименьшей, и равной " << cosx << endl;
+        }
+        else if (lnx > sinx > cosx) {
+            cout << "Функция lnx является наименьшей, и равной " << lnx << endl;
+        }
+        else {
+            cout << "Ошибка\n";
+        }
+        */
+    }
+
+
+    
 
 
 
