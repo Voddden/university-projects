3a5d6e464e0a4963e65347245e421d7109d8c5be
Даник <voddden@gmail.com>
2021-10-13 14:57:22 +0300

    Вроде закодил 1 задание

--- Laba3--task1/Laba3--task1.cpp
+++ Laba3--task1/Laba3--task1.cpp
@@ -1,7 +1,7 @@
-﻿//Лабораторная , задача . Выполнена: Воднев Д. И.
+﻿//Лабораторная 3, задача 1 Выполнена: Воднев Д. И.
 
 /*
-
+    Вычислить N
 */
 
 #include <iostream>
@@ -10,5 +10,25 @@ using namespace std;
 int main() {
     setlocale(LC_ALL, "Rus");
 
+    double N = 0, a, b;
+    int i = 1;
+
+    //cout << "Введите N\n";
+    //cin >> N;
+    while (i <= 30) {
+        if (i % 2 == 1) { // нечётное
+            a = i;
+            b = i * i;
+            N = N + (a - b) * (a - b);
+        }
+        else if (i % 2 == 0) { // чётное
+            a = i / 2;
+            b = i * i * i;
+            N = N + (a - b) * (a - b);
+        }
+        
+        ++i;
+    }
+    cout << "Ответ:\n" << N << endl;
     return 0;
 }
