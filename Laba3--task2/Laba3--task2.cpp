eff4a5332fabd541c36a54515a9f93cb38b5d009
Даник <voddden@gmail.com>
2021-10-13 15:05:46 +0300

    Закодил 2 задание. Надо тестить

--- Laba3--task2/Laba3--task2.cpp
+++ Laba3--task2/Laba3--task2.cpp
@@ -1,7 +1,7 @@
-﻿//Лабораторная , задача . Выполнена: Воднев Д. И.
+﻿//Лабораторная 3, задача 2. Выполнена: Воднев Д. И.
 
 /*
-
+    Найти суму ряда, используя цикл do-while
 */
 
 #include <iostream>
@@ -9,6 +9,12 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Rus");
-
+    double d = 1;
+    int n = 0;
+    while (d >= 0.001) {
+        d = 1 / pow(2, n) + 1 / pow(3, n);
+        ++n;
+    }
+    cout << "Ответ: " << d << endl;
     return 0;
 }
