078c2d693d0ffda8418b21295f4a93d9ca98180a
Даник <voddden@gmail.com>
2021-10-23 21:15:05 +0300

    закодил 9 задание

--- Laba3--task9/Laba3--task9.cpp
+++ Laba3--task9/Laba3--task9.cpp
@@ -1,14 +1,23 @@
-﻿//Лабораторная , задача . Выполнена: Воднев Д. И.
+﻿//Лабораторная 3, задача 9 Выполнена: Воднев Д. И.
 
 /*
-
+    Вычислить сумму последовательности
 */
 
+#include <cmath>
 #include <iostream>
 using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Rus");
-
+    cout << "Введите 1-ый и 2-ой член АП\n";
+    long long a1, a2, d;
+    cin >> a1 >> a2;
+    d = a2 - a1;
+    long long sum = 0;
+    for (long long Ix = 1; Ix <= 99999; ++Ix) { // Ix - индекс последовательности (Xn)
+        sum = sum + pow(2, Ix - 1) * pow(-1, Ix - 1) * (a1 + d * (Ix - 2));
+    }
+    cout << "Ответ:\n" << sum << endl;
     return 0;
 }
