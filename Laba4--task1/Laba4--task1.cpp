eea2953ef421a52c3fffa7bcebea986862577530
Даник <voddden@gmail.com>
2021-10-28 20:38:54 +0300

    начал делать 1 и 2 задания

--- Laba4--task1/Laba4--task1.cpp
+++ Laba4--task1/Laba4--task1.cpp
@@ -55,7 +55,7 @@ int main() {
 		}
 	}
 	delete[] arr;
-
+	
     return 0;
 }
 
--- Laba4--task2/Laba4--task2.cpp
+++ Laba4--task2/Laba4--task2.cpp
@@ -1,20 +1,59 @@
-﻿// Laba4--task2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿//Лабораторная 4, задача 2 Выполнена: Воднев Д. И.
 
+/*
+
+*/
+
+using namespace std;
 #include <iostream>
+#include <time.h>
+#include <cmath>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    setlocale(LC_ALL, "Rus");
+    srand(time(NULL));
+    int n, m;
+    cout << "Введите кол-во строк матрицы\n";
+    cin >> n;
+    while (cin.fail() || cin.peek() != '\n' || n != (long long)n || n <= 0) {
+        cin.clear();
+        cin.ignore(99999, '\n');
+        cout << "Введены некорректные данные, попробуйте ещё раз:\n";
+        cin >> n;
+    }
+    cout << "Введите кол-во столбцов матрицы\n";
+    cin >> m;
+    while (cin.fail() || cin.peek() != '\n' || m != (long long)m || m <= 0) {
+        cin.clear();
+        cin.ignore(99999, '\n');
+        cout << "Введены некорректные данные, попробуйте ещё раз:\n";
+        cin >> m;
+    }
+    int** arr = new int* [n];
+    for (int t = 0; t < n; ++t) {
+        arr[t] = new int[m];
+    }
+   
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            arr[i][j] = rand() % 10 + 1;
+        }
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    long long sum = 0, pro = 1;
+    for (int q = 0; q < min(n,m); ++q) {
+        sum += arr[q][q];
+        //pro *= arr[q][(n - 1) - q]; много багов
+    }
+    cout << "Сумма элементов главной диагонали равна " << sum << endl;
+    cout << "Произведение элементов побочной диагонали равно " << pro << endl;
+    delete[] arr;
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
