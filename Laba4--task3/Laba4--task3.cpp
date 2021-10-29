f04cbf2be185d16a0015aedc1c1509e2973d9248
Даник <voddden@gmail.com>
2021-10-29 21:44:11 +0300

    100% рабочий билд 3 задания (с ручным вводом 0 и 1)

--- Laba4--task3/Laba4--task3.cpp
+++ Laba4--task3/Laba4--task3.cpp
@@ -2,24 +2,25 @@
 
 /*
 Память для массива выделить динамически. 
-Выполнить всоответствии с номером варианта индивидуальное задание и
+Выполнить в соответствии с номером варианта индивидуальное задание и
 вывести на экран исходные данные и полученный результат.
 
 вариант 5. Задан массив размером NхN, состоящий из 0 и 1. 
-Повернуть элементы массива на 90о по часовой стрелке.
+Повернуть элементы массива на 90° по часовой стрелке.
 */
 
 using namespace std;
 #include <iostream>
+#include <ctime>
 
 int main() {
     setlocale(LC_ALL, "Rus");
-    srand(time(NULL));
+    srand(time(0));
     int n;
 
     cout << "Введите размерность массива\n";
     cin >> n;
-    while (cin.fail() || n != (long long)n || n <= 0) {
+    while (cin.fail() || cin.peek() != '\n' || n != (long long)n || n <= 0) {
         cin.clear();
         cin.ignore(99999, '\n');
         cout << "Введены некорректные данные, попробуйте ещё раз:\n";
@@ -31,13 +32,22 @@ int main() {
         arr[t] = new int[n];
     }
 
+    cout << "Введите значения элементов массива (0 или 1)\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            arr[i][j] = rand() % 2;
+            cout << "arr[" << i << "]" << "[" << j << "] = ";
+            cin >> arr[i][j];
+            while (cin.fail() || cin.peek() != '\n' || arr[i][j] != (long long)arr[i][j] || (arr[i][j] != 0 && arr[i][j] != 1)) {
+                cin.clear();
+                cin.ignore(99999, '\n');
+                cout << "Введены некорректные данные, попробуйте ещё раз:\n";
+                cout << "arr[" << i << "]" << "[" << j << "] = ";
+                cin >> arr[i][j];
+            }
         }
     }
 
-    cout << endl;
+    cout << endl << "Изначальный массив:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cout << arr[i][j] << " ";
@@ -46,7 +56,7 @@ int main() {
     }
     cout << endl;   
 
-    // поворот матрицы 
+    // поворот массива
     for (int r = 0; r < n; r++) {
         for (int c = r; c < n; c++) {
             swap(arr[r][c], arr[c][r]);
@@ -58,12 +68,13 @@ int main() {
         }
     }
     //
+    cout << "Повёрнутый на 90° по часовой стрелке массив:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cout << arr[i][j] << " ";
         }
         cout << "\n";
     }
-    cout << endl;
+
     return 0;
 }
