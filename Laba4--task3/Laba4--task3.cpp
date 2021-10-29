1635dfb857f66305be7174e1381654b40b2b0977
Даник <voddden@gmail.com>
2021-10-29 21:15:37 +0300

    рабочий билд 3 задания (с автозаполнением); небольшие правки в 4

--- Laba4--task3/Laba4--task3.cpp
+++ Laba4--task3/Laba4--task3.cpp
@@ -46,5 +46,24 @@ int main() {
     }
     cout << endl;   
 
+    // поворот матрицы 
+    for (int r = 0; r < n; r++) {
+        for (int c = r; c < n; c++) {
+            swap(arr[r][c], arr[c][r]);
+        }
+    }
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < n / 2; c++) {
+            swap(arr[r][c], arr[r][n - c - 1]);
+        }
+    }
+    //
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    cout << endl;
     return 0;
 }
--- Laba4--task4/Laba4--task4.cpp
+++ Laba4--task4/Laba4--task4.cpp
@@ -1,4 +1,4 @@
-﻿//Лабораторная , задача  Выполнена: Воднев Д. И.
+﻿//Лабораторная 4, задача 4 Выполнена: Воднев Д. И.
 
 /*
 
@@ -9,9 +9,9 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Rus");
-
     srand(time(NULL));
     int n, m;
+
     cout << "Введите кол-во строк матрицы\n";
     cin >> n;
     while (cin.fail() || cin.peek() != '\n' || n != (long long)n || n <= 0) {
@@ -20,6 +20,7 @@ int main() {
         cout << "Введены некорректные данные, попробуйте ещё раз:\n";
         cin >> n;
     }
+
     cout << "Введите кол-во столбцов матрицы\n";
     cin >> m;
     while (cin.fail() || cin.peek() != '\n' || m != (long long)m || m <= 0) {
@@ -28,6 +29,7 @@ int main() {
         cout << "Введены некорректные данные, попробуйте ещё раз:\n";
         cin >> m;
     }
+
     int** arr = new int* [n];
     for (int t = 0; t < n; ++t) {
         arr[t] = new int[m];
@@ -39,13 +41,14 @@ int main() {
         }
     }
 
+    cout << endl;
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
             cout << arr[i][j] << " ";
         }
         cout << "\n";
     }
+    cout << endl;
 
     return 0;
 }
-
