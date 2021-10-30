23f194e1548638d24fe9cda4d9cfb3de3e82a82c
Даник <voddden@gmail.com>
2021-10-30 09:38:12 +0300

    Начал делать 4 через функции

--- Laba4--task4/Laba4--task4.cpp
+++ Laba4--task4/Laba4--task4.cpp
@@ -7,6 +7,16 @@
 using namespace std;
 #include <iostream>
 
+//int isLocalMinimum(int a = arr[i][j], int b = arr[i][j + 1], int c = arr[i - 1][j], int d = arr[i][j - 1], int e = arr[i + 1][j])
+int isLocalMinimum(int a, int b = 0, int c = 0, int d = 0, int e = 0) {
+    if (a < b && a < c && a < d && a < c) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
+
 int main() {
     setlocale(LC_ALL, "Rus");
     srand(time(NULL));
@@ -50,6 +60,21 @@ int main() {
     }
     cout << endl;
 
+    // начало алгоритма
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            // перебор элементов для нахождения минимума
+            if (i > 0 && i < n) {
+                sum += isLocalMinimum(arr[i][j], arr[i][j + 1], arr[i - 1][j], arr[i][j - 1], arr[i + 1][j]);
+            }
+            else {
+                cout << 1;
+            }
+            
+        }
+    }
+    cout << "Сумма равна:\n" << sum;
     delete[] arr;
     return 0;
 }
