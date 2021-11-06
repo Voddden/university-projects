36a7266a3d648bcb23f9978074ade04770917443
Даник <voddden@gmail.com>
2021-11-06 15:34:24 +0300

    изменения

--- Laba4--task13/Laba4--task13.cpp
+++ Laba4--task13/Laba4--task13.cpp
@@ -1,20 +1,33 @@
-﻿// Laba4--task13.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+﻿//Лабораторная 4, задача 13 Выполнена: Воднев Д. И.
 
+/*
+У Миши в общежитии всего лишь 1 розетка. Дабы исправить это
+недоразумение, Миша купил N удлинителей таких, что i-й удлинитель имеет
+a[i] входов. Вычислите, сколько розеток получится у Миши, если он
+оптимально соединит удлинители?
+*/
+
+using namespace std;
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    setlocale(LC_ALL, "Rus");
+    ios_base::sync_with_stdio(0);
+    int N;
+    int sum = 0;
+
+    cout << "Введите количество удлинителей:\n";
+    cin >> N;
+    while (cin.fail() || cin.peek() != '\n' || N != (long long)N || N <= 0) {
+        cin.clear();
+        cin.ignore(99999, '\n');
+        cout << "Введены некорректные данные, попробуйте ещё раз:\n";
+        cin >> N;
+    }
+    for (int i = 2; i <= N; ++i) {
+        sum += (i - 1);
+    }
+    cout << "Кол-во розеток равно:\n" << sum + 1 << "\n\n";
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
