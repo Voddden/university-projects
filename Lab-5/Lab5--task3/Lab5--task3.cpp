ed0a813097a90262c069bd9cd21dff31589211f9
Даник <voddden@gmail.com>
2021-11-17 22:09:51 +0300

    добавление папок debug в файлы 5 лабы

--- Lab-5/Lab5--task3/Lab5--task3.cpp
+++ Lab-5/Lab5--task3/Lab5--task3.cpp
@@ -7,6 +7,7 @@ int main()
 {
     std::cout << "Hello World!\n";
     std::cout << 100;
+    std::cout << "ya chozhu pitsy\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
