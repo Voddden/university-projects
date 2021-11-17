b1eea27ac08870e1d93130522f520649d370ab57
Даник <voddden@gmail.com>
2021-11-17 22:05:47 +0300

    добавление папки debug вщ все файлы заданий

--- Lab-4/Lab4--taskz12/Lab4--taskz12.cpp
+++ Lab-4/Lab4--taskz12/Lab4--taskz12.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu2
 // Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
--- Lab-4/Lab4--taskz13/Lab4--taskz13.cpp
+++ Lab-4/Lab4--taskz13/Lab4--taskz13.cpp
@@ -8,7 +8,7 @@ int main()
     std::cout << "Hello World!\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu33
 // Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
