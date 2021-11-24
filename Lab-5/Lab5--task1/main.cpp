748c22c04b7388a72489ec68bb38555079736c3e
Даник <voddden@gmail.com>
2021-11-24 18:23:41 +0300

    Л5-1: есть ввод размерностей массивой X и Y

--- Lab-5/Lab5--task1/main.cpp
+++ Lab-5/Lab5--task1/main.cpp
@@ -14,33 +14,72 @@ int main() {
 	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 
-	const int n = 3, m = 3; // размерность массива Z
+	// 1
+	cout << "¬ведите размерность 2-мерного массива Z:\n";
+	int n, m;
+	cin >> n >> m;
+	while (cin.fail() || cin.peek() != '\n' || n != (long long)n || n <= 0 || m != (long long)m || m <= 0) {
+		cin.clear();
+		cin.ignore(99999, '\n');
+		cout << "¬ведены некорректные данные, попробуйте ещЄ раз:\n";
+		cin >> n >> m;
+	}
 	int** Z = new int* [n];
 	for (int t = 0; t < n; ++t) {
 		Z[t] = new int[n];
 	}
 
-	const int x = 5; // размерность массива X 
-	int** X = new int* [x];
+	
+	// 2
+	cout << "¬ведите размерность 1-мерного массива X:\n";
+	int x;
+	cin >> x;
+	while (cin.fail() || cin.peek() != '\n' || x != (long long)x || x <= 0) {
+		cin.clear();
+		cin.ignore(99999, '\n');
+		cout << "¬ведены некорректные данные, попробуйте ещЄ раз:\n";
+		cin >> x;
+	}
+	int* X = new int [x];
 
-	const int y = 5; // размерность массива Y
-	int** Y = new int* [y];
+	// 3
+	cout << "¬ведите размерность 1-мерного массива Y:\n";
+	int y;
+	cin >> y;
+	while (cin.fail() || cin.peek() != '\n' || y != (long long)y || y <= 0) {
+		cin.clear();
+		cin.ignore(99999, '\n');
+		cout << "¬ведены некорректные данные, попробуйте ещЄ раз:\n";
+		cin >> y;
+	}
+	int* Y = new int [y];
 
-
-
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			Z[i][j] = X[i] * Y[j];
+	//X
+	for (int i = 0; i < x; ++i) {
+		cout << "X[" << i << "] = ";
+		cin >> X[i];
+		while (cin.fail() || cin.peek() != '\n' || X[i] != (long long)X[i]) {
+			cin.clear();
+			cin.ignore(99999, '\n');
+			cout << "¬ведены некорректные данные, попробуйте ещЄ раз:\n";
+			cout << "X[" << i << "] = ";
+			cin >> X[i];
 		}
 	}
-	cout << endl;
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			cout << Z[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << endl;
+
+	//for (int i = 0; i < n; ++i) {
+	//	for (int j = 0; j < n; ++j) {
+	//		Z[i][j] = X[i] * Y[j];
+	//	}
+	//}
+	//cout << endl;
+	//for (int i = 0; i < n; ++i) {
+	//	for (int j = 0; j < n; ++j) {
+	//		cout << Z[i][j] << " ";
+	//	}
+	//	cout << "\n";
+	//}
+	//cout << endl;
 
 
 
