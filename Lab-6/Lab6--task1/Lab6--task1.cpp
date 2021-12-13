1fcbb1505a5dbd307bf52d0dcf0fe0a2f40ebeb9
Даник <voddden@gmail.com>
2021-12-13 13:31:08 +0300

    Л6-1: развёрнутый рабочий билд

--- Lab-6/Lab6--task1/Lab6--task1.cpp
+++ Lab-6/Lab6--task1/Lab6--task1.cpp
@@ -7,14 +7,16 @@
 
 using namespace std;
 #include <iostream>
+#include <iomanip>
 #include <cstring>
 
 int main() {
     setlocale(LC_ALL, "Rus");
     cout << "Введите строку:\n";
-    const int max = 10;
-    char str[max];
-    cin.get(str, max);
+    const int max_str = 81;
+    const int max_arr = 30;
+    char str[max_str];
+    cin.get(str, max_str);
 
     //cout << "Ваша строка:\n" << str << "\n";
     // ---- #1 кол-во групп
@@ -22,47 +24,129 @@ int main() {
     for (int i = 0; i < strlen(str); ++i) {
         if (str[i] == ' ') ++groups;
     }
-    cout << "Кол-во групп == " << groups;
+    cout << "Кол-во групп: " << groups;
     // --
 
-    // ---- #2 кол-во элем-ов наибольшей группы
-    
-    // --
+
     int m = groups;
-    int n = max;
+    int n = max_arr;
     int** arr = new int* [m];
     for (int i = 0; i < m; ++i) {
         arr[i] = new int[n];
     }
     for (int i = 0; i < m; ++i) {
         for (int j = 0; j < n; ++j) {
-            arr[i][j] = 0;
+            arr[i][j] = 3;
         }
     }
-    // массив количества элементов каждой из групп
 
-    const int arr1_x = groups;
-    int arr1[arr1_x];
+    // ---- заполнение массива arr группами
+    int o = 0;
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; str[o] != ' ' && str[o] != '\0'; ++j) {
+            arr[i][j] = str[o] - 48;
+            ++o;
+        }
+        ++o;
+    }
+    // --
+    
+    // ---- создание массива из количеств элементов массива arr
+
+    int* arr1 = new int[groups];
     for (int i = 0; i < groups; ++i) {
         arr1[i] = 0;
     }
-    //
-    int j = 0;
-    for (int i = 0; i < groups; ++i) {
-        while (str[j] != ' ') {
-            arr[i][j] = str[j] - 48;
-            ++j;
+    int y = 0;
+    for (int i = 0; i < m; ++i) {
+        y = 0;
+        while (arr[i][y] != 3) {
+            ++arr1[i];
+            ++y;
         }
-        ++j;
     }
+
+    // перебор массива arr1: подсчёт кол-ва нечётных элементов (групп)
+    int q = 0; // счётчик кол-ва нечётных групп
+    for (int i = 0; i < groups; ++i) {
+        if (arr1[i] % 2 == 1) ++q;
+    }
+
+
+    cout << "\nКол-во элементов групп: ";
+    for (int i = 0; i < groups; ++i) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\nКол-во нечётных групп: " << q;
+
+    // --
+
+    // ---- создание массива из групп с нечётным кол-вом символов
     
-    //
-    //cout << "\nМассив arr:\n";
+
+    //int** arr = new int* [m];
     //for (int i = 0; i < m; ++i) {
-    //    for (int j = 0; j < n; ++j) {
-    //        cout << arr[i][j] << " ";
-    //    }
-    //    cout << endl;
+    //    arr[i] = new int[n];
     //}
+
+    int** arr2 = new int* [q];
+    for (int i = 0; i < q; ++i) {
+        arr2[i] = new int[max_arr];
+    }
+
+    for (int i = 0; i < q; ++i) {
+        for (int j = 0; j < max_arr; ++j) {
+            arr2[i][j] = 3;
+        }
+    }
+
+    for (int i = 0; i < groups; ++i) {
+        if (arr1[i] % 2 == 1) arr2[i] = arr[i];
+    }
+
+    // ---- подсчёт кол-ва единиц в нечётных группах
+
+    int* arr3 = new int[q];
+    for (int i = 0; i < q; ++i) {
+        arr3[i] = 0;
+    }
+    int e = 0;
+    for (int i = 0; i < q; ++i) {
+        //e = 0;
+        //while (arr2[i][e] != 3) {
+        //    if (arr2[i][e] == 1) ++arr3[i];
+        //    ++e;
+        //}
+
+        for (int j = 0; arr2[i][j] != 3; ++j) {
+            if (arr2[i][j] % 2 == 1) ++arr3[i];
+        }
+    }
+
+    // --
+
+    // --
+    cout << "\nМассив arr:\n";
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) {
+            cout << setw(2) << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+    cout << "Массив нечётного кол-ва групп:\n";
+    for (int i = 0; i < q; ++i) {
+        for (int j = 0; j < max_arr; ++j) {
+            cout << setw(2) << arr2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+
+    cout << "\narr3:\n";
+    for (int i = 0; i < q; ++i) {
+        cout << setw(2) << arr3[i] << " ";
+    }
     return 0;
 }
