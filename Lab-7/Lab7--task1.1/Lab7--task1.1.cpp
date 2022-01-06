b119a6cd77ca6265d997b9457c003cb4638ffc75
Даник <voddden@gmail.com>
2022-01-06 17:50:53 +0300

    save

--- Lab-7/Lab7--task1.1/Lab7--task1.1.cpp
+++ Lab-7/Lab7--task1.1/Lab7--task1.1.cpp
@@ -9,31 +9,61 @@ using namespace std;
 #include <string>
 
 int num13_to_num10(string num13) {
-    int* arr = new int[num13.length()];
-    //for (int i = 0; i < num13.length(); ++i) {
-    //    arr[i] = 0;
-    //}
-    int razryad = 0;
+    int* arr = new int[num13.length()]; // массив цифр числа num13
+
     for (int i = 0; i < num13.length(); ++i) {
         if (num13[i] == 'a') {
-            num13[i] = 10;
+            arr[i] = 10;
         }
         else if (num13[i] == 'b') {
-            num13[i] = 11;
+            arr[i] = 11;
         }
         else if (num13[i] == 'c') {
-            num13[i] = 12;
+            arr[i] = 12;
         }
         else {
-            arr[i] = num13[i];
+            arr[i] = num13[i] - 48;
         }
-        
     }
-    return 0;
+
+    int num10 = 0;
+    int razryad = 0;
+    for (int i = num13.length() - 1; i >= 0; --i) { // преобразование к 10-чной СС
+        num10 = num10 + arr[i] * pow(13, razryad);
+        ++razryad;
+    }
+
+    return num10;
 }
 
 int num10_to_num3(int num10) {
-    return 0;
+    int number_of_digits_in_num3 = 1;
+    int num10_1 = num10;
+    while (num10_1 / 3 > 0) {
+        ++number_of_digits_in_num3;
+        num10_1 = num10_1 / 3;
+    }
+
+    int* arr = new int[number_of_digits_in_num3]; // массив цифр num3 (задом наперед)
+    for (int i = 0; i < number_of_digits_in_num3; ++i) {
+        arr[i] = num10 % 3;
+        num10 /= 3;
+    }
+
+    int c;
+    for (int i = 0; i < (number_of_digits_in_num3 / 2); i++) { // переворачивание массива цифр num3
+        c = arr[i];
+        arr[i] = arr[number_of_digits_in_num3 - 1 - i];
+        arr[number_of_digits_in_num3 - 1 - i] = c;
+    }
+
+    int num3 = 0;
+    int razryad = number_of_digits_in_num3 - 1;
+    for (int i = 0; i < number_of_digits_in_num3; ++i) { // перевод числа num3 из массивного представления в числовое
+        num3 = num3 + arr[i] * pow(10, razryad);
+        --razryad;
+    }
+    return num3;
 }
 
 int main() {
@@ -41,9 +71,10 @@ int main() {
     cout << "---Введите число в 13-чной системе счисления: ";
     string num13;
     getline(cin, num13);
+
     int num10 = num13_to_num10(num13);
-    cout << "Число в 10-чной СС: " << num10 << endl;
-    //int num3 = num10_to_num3(num10);
-    //cout << "Число в 3-чной системе счисления: " << num3 << endl;
+    //cout << "Число в 10-чной СС: " << num10 << endl;
+    int num3 = num10_to_num3(num10);
+    cout << "Число в 3-чной системе счисления: " << num3 << endl;
     return 0;
 }
