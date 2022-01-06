5e505e050b0269c1d662345693b032c666c85689
Даник <voddden@gmail.com>
2022-01-06 16:26:59 +0300

    save

--- Lab-7/Lab7--task1.1/Lab7--task1.1.cpp
+++ Lab-7/Lab7--task1.1/Lab7--task1.1.cpp
@@ -1,78 +1,49 @@
 // Лабораторная 7, задача 1(вариант 5). Выполнена: Воднев Д. И.
 
-/* из девятичной в семнадцатеричную (с массивом) 
-* 
-* 
-*  
-
+/* 
+    перевод из 13-чной системы счисления в 3-чную 
 */
 
 using namespace std;
 #include <iostream>
+#include <string>
 
-int input_num() {
-    while (true) {
-        int value;
-
-        cin >> value;
-
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(32767, '\n');
-            cout << "Неккоректный ввод\nВведите число в девятичной системе счисления(цифры от 0 до 9): ";
+int num13_to_num10(string num13) {
+    int* arr = new int[num13.length()];
+    //for (int i = 0; i < num13.length(); ++i) {
+    //    arr[i] = 0;
+    //}
+    int razryad = 0;
+    for (int i = 0; i < num13.length(); ++i) {
+        if (num13[i] == 'a') {
+            num13[i] = 10;
+        }
+        else if (num13[i] == 'b') {
+            num13[i] = 11;
+        }
+        else if (num13[i] == 'c') {
+            num13[i] = 12;
         }
         else {
-            cin.ignore(32767, '\n');
-            return value;
+            arr[i] = num13[i];
         }
+        
     }
+    return 0;
+}
+
+int num10_to_num3(int num10) {
+    return 0;
 }
 
 int main() {
     setlocale(LC_ALL, "rus");
-    cout << "Программа осуществляет перевод чисел из девятичной в семнадцатеричную систему счисления (с массивом).";
-    cout << "\nВведите число в девятичной системе счисления: ";
-    int num = input_num(); //число в девятичной сс
-    int n = num;
-    int count = 0, add; //кол-во цифр в числе
-
-    int count_1 = 0, ten = 0;;
-    do {
-        int d = num % 10;
-        num /= 10;
-
-        ten += d * powl(9, count_1);
-
-        count_1++;
-        count--;
-    } while (num);
-    cout << "Число в десятичной сс: " << ten << endl;
-
-    int k1 = 10, k2 = 11, t = 8, z, i, ss = 17;
-    char ms[20];
-    z = ten;
-    ten -= z;
-    while (z >= ss)
-    {
-        i = z % ss;
-        z /= ss;
-        ms[k1--] = i > 9 ? i - 10 + 'A' : i + '0';
-    }
-    ms[k1--] = z > 9 ? z - 10 + 'A' : z + '0';
-    ms[k1] = ' ';
-    ms[k2++] = '.';
-    while (ten != 0 && k2 - 11 <= t) {
-        ten *= ss;
-        i = ten;
-        ms[k2++] = i > 9 ? i - 10 + 'A' : i + '0';
-        ten -= i;
-    }
-    cout << "\nВаше число:";
-    for (i = k1; i < k2; i++)
-        cout << ms[i];
-
-
-    cout << endl;
-
+    cout << "---Введите число в 13-чной системе счисления: ";
+    string num13;
+    getline(cin, num13);
+    int num10 = num13_to_num10(num13);
+    cout << "Число в 10-чной СС: " << num10 << endl;
+    //int num3 = num10_to_num3(num10);
+    //cout << "Число в 3-чной системе счисления: " << num3 << endl;
     return 0;
 }
